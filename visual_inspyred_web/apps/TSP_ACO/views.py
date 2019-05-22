1eb09a372f09fcbbcc0ac29587e0e72a87d6463d
Jintao_Qu <00002@protonmail.com>
2019-05-22 14:22:39 +0800

    overview ok

--- visual_inspyred_web/apps/TSP_ACO/views.py
+++ visual_inspyred_web/apps/TSP_ACO/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def get_tsp_aco_form(request):
+    if request.method == "POST":
+        post_dict = {}
+        post_dict["dataset"] = request.POST.get("dataset", 'att48.tsp')
+        post_dict["populations"] = request.POST.get("populations", '20')
+        post_dict["iterations"] = request.POST.get("iterations", '100')
+        print(post_dict)
     return render(request, 'tsp_aco_form.html')
